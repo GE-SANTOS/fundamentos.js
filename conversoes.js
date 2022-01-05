dec394f636799372bb92d0dc2711e7863fa63ef2
Geovane Santos <geovane_n.s@outlook.com>
2022-01-05 17:46:11 -0300

    códigos var e conversões

---
+++ conversoes.js
@@ -0,0 +1,23 @@
+// tipo de dado
+// booleanos
+
+// conversão implícita 
+
+const numero = 456;
+const numeroString = "456";
+
+console.log(numero == numeroString);
+
+console.log(numero + numeroString);
+
+// Para realizar a conversão de String para número
+// ou número para String use os comandos abaixo.
+// Number()
+// String()
+
+console.log(numero + Number(numeroString));
+
+// conversão explícita
+//const numero = 456;
+//const numeroString = Number("456");
+//const numeroString = Number("456a"); -> NaN
