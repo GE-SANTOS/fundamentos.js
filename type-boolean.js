0e772936d2b91ee9714e8a448cdcdc2d10bb651e
Geovane Santos <geovane_n.s@outlook.com>
2022-01-05 15:30:11 -0300

    exemplos da aula1

---
+++ type-boolean.js
@@ -0,0 +1,14 @@
+// boolean
+// true -> verdadeiro
+// false -> falso
+
+const primeiroNome = 5;
+const segundoNome = 10;
+let cadastroAtivado = true;
+
+console.log(primeiroNome === segundoNome);
+
+const texto1 = "Alura";
+const texto2 = "alura";
+
+console.log(texto1 === texto2);
---
+++ type-string.js
@@ -0,0 +1,16 @@
+const texto1 = "Olá, Mundo!";
+const texto2 = 'Olá, Mundo!';
+const senha = "senhaSuperSegura456!";
+const StringDeNumeros = "34567";
+
+/*O mesmo funciona se as aspas simples
+ou dupla estiverem em ondem diferentes*/
+
+const citacao = "Meu nome é ";
+const meuNome = "Geovane";
+
+// concatenação (+)
+
+console.log(citacao + meuNome);
+
+// Pesquisar template string ou template literal
